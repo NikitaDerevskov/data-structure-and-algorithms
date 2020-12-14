a2cd7e388bca932e8315a19a51091bdead9dc41e
Nikita Derevskov <nikitaderevskov@gmail.com>
2020-12-14 21:50:48 +0300

    write tests to one-dimension-array task

--- __tests__/base.spec.ts
+++ __tests__/base.spec.ts
@@ -1,7 +1,12 @@
-import { sampleFunction } from "../solutions/one-demension-array";
+import { longestSequence } from "../solutions/one-dimension-array";
 
-describe("Simple expression tests", () => {
-    test("First check", () => {
-        expect(sampleFunction('hello')).toEqual('hellohello');
+describe("Test tasks", () => {
+    test("One dimension array task", () => {
+        expect(longestSequence([1, 1, 0, 1, 1, 0, 1, 0, 1, 1])).toBe(5);
+        expect(longestSequence([1, 1, 0, 1, 1, 1, 1, 0, 1, 1])).toBe(7);
+        expect(longestSequence([1, 1, 1, 1])).toBe(4);
+        expect(longestSequence([0, 0, 0, 0])).toBe(1);
+        expect(longestSequence([0])).toBe(1);
+        expect(longestSequence([])).toBe(0);
     });
 });
